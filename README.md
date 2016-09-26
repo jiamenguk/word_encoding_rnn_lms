8acfe64b28809d9d1f328555c7f3d4c4e7dedda3
Jiameng Gao <jiamenggao@jg.local>
2016-09-27 00:24:04 +0100

    added readme and tidied up

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# word_encoding_rnn_lms
+# Setup
+
+Install OpenFST and its Python extension following instructions at [OpenFST](http://www.openfst.org/twiki/bin/view/FST/PythonExtension "OpenFST")
+
+Install the required numpy and TensorFlow at using:
+
+`pip install requirements.txt`
+
+# Running encoding scripts
+
+For each encoding scheme, there is a `read_file.py` which takes an input in the first system argument and spits out a mapping of the words in the input file to the decomposed subword units in a hash table. This is stored in JSON as a dict.
+
+`python $SCHEME/read_file.py ../data/de/wmt15.de`
+
+Note that for BPE this is done using the `learn_bpe.py` file
+
+The `squash.py` takes in an input file and the dictionary mapping to output the encoded text.
+
+This output file can then be used to train LM models
+
+~~~
+cd lm_train
+python ptb_word_lm.py --data_path ../data/de/wmt15.de.$SCHEME --train_path ./$SCHEME/ 
+~~~
+
+This also outputs a file in IDs and the mapping of the words (`.ids` file) to the IDs in the `.wmap` file.
+
+The `nbest_mapper.py` file in the encoding scripts take these `.wmap` files and the UCAM-SMT mapping files and creates a mapping from the UCAM word IDs to the subword IDs. The script then opens directories of N-Best outputs from the OpenFST lattice and converts them into the subword IDs. (This section may some times be commented out) (To be extended)
+
+There may also be `FST_Conv.py` scripts which builds a mapping transducer, same to that described `nbest_mapper.py` and outputs transformed lattices.
+
+# Rescoring
+
+N-best rescoring is done by running 
+
+~~~
+cd lm_train
+python ptb_word_lm.py --data_path $PATH_TO_MAPPED_NBEST_OUTPUTS --train_path ./$SCHEME/ --wmap_path ../data/de/wmt15.de.$SCHEME.wmap --outfile ./nbest-$SCHEME/
+~~~
--- bpe/read_file.py
+++ bpe/read_file.py
@@ -1,103 +1,252 @@
-import sys, json, re, unicodedata, os
-from jutils import tokenizer
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Rico Sennrich
 
-import collections
+"""Use byte pair encoding (BPE) to learn a variable-length encoding of the vocabulary in a text.
+Unlike the original BPE, it does not compress the plain text, but can be used to reduce the vocabulary
+of a text to a configurable number of symbols, with only a small increase in the number of tokens.
 
-unksym = "<unk>"
+Reference:
+Rico Sennrich, Barry Haddow and Alexandra Birch (2015). Neural Machine Translation of Rare Words with Subword Units.
+"""
 
-vocab = collections
-MAX_SIZE = 50000
-SYM_SIZE = 100
+from __future__ import unicode_literals
+
+import sys
+import codecs
+import re
+import json
+import os
+import copy
+import argparse
+from collections import defaultdict, Counter
+from jutils import tokenizer_norm as tokenizer
+
+# hack for python2/3 compatibility
+from io import open
+argparse.open = open
+
+# python 2/3 compatibility
+if sys.version_info < (3, 0):
+  sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
+  sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+  sys.stdin = codecs.getreader('UTF-8')(sys.stdin)
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="learn BPE-based word segmentation")
+
+    parser.add_argument(
+        '--input', '-i', type=argparse.FileType('r'), default=sys.stdin,
+        metavar='PATH',
+        help="Input text (default: standard input).")
+    parser.add_argument(
+        '--output', '-o', type=argparse.FileType('w'), default=sys.stdout,
+        metavar='PATH',
+        help="Output file for BPE codes (default: standard output)")
+    parser.add_argument(
+        '--symbols', '-s', type=int, default=30000,
+        help="Create this many new symbols (each representing a character n-gram) (default: %(default)s))")
+    parser.add_argument(
+        '--verbose', '-v', action="store_true",
+        help="verbose mode.")
+
+    return parser
 
 def build_vocab(line):
-	counter = collections.Counter(line)
-	count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
-	return count_pairs
+    counter = Counter(line)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    return count_pairs
 
-def append_vocab(vocab):
-	new = []
-	for item in vocab:
-		new.append((list(item[0]+"."),item[1]))
-	return new
+def get_vocabulary(fobj):
+    """Read text and return dictionary that encodes vocabulary
+    """
+    # vocab = Counter()
+    # for i, line in enumerate(fobj):
+    #     for word in line.split():
+    #         vocab[word] += 1
+    # return vocab
+    vocab = Counter()
+    for i, line in enumerate(fobj):
+        line = tokenizer(line)
+        vocab_line = build_vocab(line)
+        for item in vocab_line:
+            vocab[item[0]] += item[1]
+        if (i % 100000) == 0:
+            print i
+    # with open(args.input+".vocab.bpe",'w') as outfile:
+    #     outfile.write(json.dumps(vocab))
 
-def break_down(item):
-	p1 = zip(item[:-1],item[1:])
-	pairs = ['_'.join(i) for i in p1]
-	return pairs
+    return vocab
 
-def make_list(changed):
-	i = 0
-	pairs = collections.Counter('')
-	for item in changed:
-		word = break_down(item[0])
-		for a in word:
-			pairs[a] += item[1]
-	return pairs
+def update_pair_statistics(pair, changed, stats, indices):
+    """Minimally update the indices and frequency of symbol pairs
 
-def vocab_update(vocab, new_pair, joined):
-	new_vocab = vocab
-	changed = []
-	for i,item in enumerate(vocab):
-		word = '_'.join(item[0])
-		pattern = re.compile(r'(?<!\S)' + re.escape(word) + r'(?!\S)')
-		if new_pair in word:
-			i1 = word.replace(new_pair,joined).split('_')
-			_tmp = (i1,item[1])
-			changed.append(_tmp)
-			new_vocab[i] = _tmp
-	return new_vocab, changed
+    if we merge a pair of symbols, only pairs that overlap with occurrences
+    of this pair are affected, and need to be updated.
+    """
+    stats[pair] = 0
+    indices[pair] = defaultdict(int)
+    first, second = pair
+    new_pair = first+second
+    for j, word, old_word, freq in changed:
 
-def prune(pairs):
-	for item,freq in list(pairs.items()):
-		if freq < 3:
-			del pairs[item]
+        # find all instances of pair, and update frequency/indices around it
+        i = 0
+        while True:
+            try:
+                i = old_word.index(first, i)
+            except ValueError:
+                break
+            if i < len(old_word)-1 and old_word[i+1] == second:
+                if i:
+                    prev = old_word[i-1:i+1]
+                    stats[prev] -= freq
+                    indices[prev][j] -= 1
+                if i < len(old_word)-2:
+                    # don't double-count consecutive pairs
+                    if old_word[i+2] != first or i >= len(old_word)-3 or old_word[i+3] != second:
+                        nex = old_word[i+1:i+3]
+                        stats[nex] -= freq
+                        indices[nex][j] -= 1
+                i += 2
+            else:
+                i += 1
 
-if os.path.exists(sys.argv[1]+".vocab"):
-	vocab = json.load(open(sys.argv[1]+".vocab",'r'))
-else:
-	vocab = collections.Counter('')
-	with open(sys.argv[1],'r') as infile:
-		for i, line in enumerate(infile):
-			line = tokenizer(line)
-			vocab_line = build_vocab(line)
-			for item in vocab_line:
-				vocab[item[0]] += item[1]
-			if (i % 100000) == 0:
-				print i
-	with open(sys.argv[1]+".vocab",'w') as outfile:
-		outfile.write(json.dumps(vocab))
+        i = 0
+        while True:
+            try:
+                i = word.index(new_pair, i)
+            except ValueError:
+                break
+            if i:
+                prev = word[i-1:i+1]
+                stats[prev] += freq
+                indices[prev][j] += 1
+            # don't double-count consecutive pairs
+            if i < len(word)-1 and word[i+1] != new_pair:
+                nex = word[i:i+2]
+                stats[nex] += freq
+                indices[nex][j] += 1
+            i += 1
 
-print len(vocab.keys())
 
-vocab = sorted(vocab.items(), key=lambda x: (-x[1], x[0]))
-vocab = append_vocab(vocab)
-new_pairs = set('abcdefghijklmnopqrstuvwxyz.')
-pairs = dict()
-changed = vocab
-# bpe_dict = list(units)
-gather = dict()
-for i in range(0, 3000):
-	new_dict = make_list(changed)
-	pairs.update(new_dict)
-	# count_pairs = sorted(pairs.items(), key=lambda x: -x[1])
-	new_pair = max(pairs, key=pairs.get)
-	if pairs[new_pair] < 2 or len(pairs.keys())==0:
-		break
-	threshold = pairs[new_pair] * i/(i+10000.0)
-	gather[new_pair] = pairs[new_pair]
-	pairs[new_pair] = 0
-	# bpe_dict.append(new_pair)
-	tmp = new_pair.replace('_','')
-	vocab, changed = vocab_update(vocab, new_pair, tmp)
-	if (i % 100) == 0:
-		prune(pairs)
-		print i
-		# print pairs
+def get_pair_statistics(vocab):
+    """Count frequency of all symbol pairs, and create index"""
+
+    # data structure of pair frequencies
+    stats = defaultdict(int)
+
+    #index from pairs to words
+    indices = defaultdict(lambda: defaultdict(int))
+
+    for i, (word, freq) in enumerate(vocab):
+        prev_char = word[0]
+        for char in word[1:]:
+            stats[prev_char, char] += freq
+            indices[prev_char, char][i] += 1
+            prev_char = char
+
+    return stats, indices
+
+
+def replace_pair(pair, vocab, indices):
+    """Replace all occurrences of a symbol pair ('A', 'B') with a new symbol 'AB'"""
+    first, second = pair
+    pair_str = ''.join(pair)
+    pair_str = pair_str.replace('\\','\\\\')
+    changes = []
+    pattern = re.compile(r'(?<!\S)' + re.escape(first + ' ' + second) + r'(?!\S)')
+    if sys.version_info < (3, 0):
+        iterator = indices[pair].iteritems()
+    else:
+        iterator = indices[pair].items()
+    for j, freq in iterator:
+        if freq < 1:
+            continue
+        word, freq = vocab[j]
+        new_word = ' '.join(word)
+        new_word = pattern.sub(pair_str, new_word)
+        new_word = tuple(new_word.split())
+
+        vocab[j] = (new_word, freq)
+        changes.append((j, new_word, word, freq))
+
+    return changes
+
+def prune_stats(stats, big_stats, threshold):
+    """Prune statistics dict for efficiency of max()
+
+    The frequency of a symbol pair never increases, so pruning is generally safe
+    (until we the most frequent pair is less frequent than a pair we previously pruned)
+    big_stats keeps full statistics for when we need to access pruned items
+    """
+    for item,freq in list(stats.items()):
+        if freq < threshold:
+            del stats[item]
+            if freq < 0:
+                big_stats[item] += freq
+            else:
+                big_stats[item] = freq
+
+if __name__ == '__main__':
+
+    parser = create_parser()
+    args = parser.parse_args()
+
+    # vocab = get_vocabulary(args.input)
+    vocab = json.load(open("../data/de/wmt15.vocab",'r'))
+    vocab = dict([(tuple(x)+('</w>',) ,y) for (x,y) in vocab.items()])
+    sorted_vocab = sorted(vocab.items(), key=lambda x: x[1], reverse=True)
+    vocab_map = copy.deepcopy(sorted_vocab)
+
+    stats, indices = get_pair_statistics(sorted_vocab)
+    big_stats = copy.deepcopy(stats)
+    gather = dict()
+    # threshold is inspired by Zipfian assumption, but should only affect speed
+    threshold = max(stats.values()) / 10
+    for i in range(args.symbols):
+        if stats:
+            most_frequent = max(stats, key=stats.get)
+
+        # we probably missed the best pair because of pruning; go back to full statistics
+        if not stats or (i and stats[most_frequent] < threshold):
+            prune_stats(stats, big_stats, threshold)
+            stats = copy.deepcopy(big_stats)
+            most_frequent = max(stats, key=stats.get)
+            # threshold is inspired by Zipfian assumption, but should only affect speed
+            threshold = stats[most_frequent] * i/(i+10000.0)
+            prune_stats(stats, big_stats, threshold)
+
+        if stats[most_frequent] < 2:
+            sys.stderr.write('no pair has frequency > 1. Stopping\n')
+            break
+
+        if args.verbose:
+            sys.stderr.write('pair {0}: {1} {2} -> {1}{2} (frequency {3})\n'.format(i, most_frequent[0], most_frequent[1], stats[most_frequent]))
+        args.output.write('{0} {1}\n'.format(*most_frequent))
+        gather[''.join(most_frequent)] = stats[most_frequent]
+        changes = replace_pair(most_frequent, sorted_vocab, indices)
+        update_pair_statistics(most_frequent, changes, stats, indices)
+        stats[most_frequent] = 0
+        if not i % 100:
+            prune_stats(stats, big_stats, threshold)
+
+        if not i % 1000:
+            print i
 
 bpe_dict = {}
-for item in vocab:
-	word = ''.join(item[0])
-	bpe_dict[word] = item[0]
+for item in sorted_vocab:
+    word = ''.join(item[0]).encode('utf-8')
+    word = unicode(word, 'ascii', errors='replace')
+    bpe_dict[word] = item[0]
 
-with open("dict.bpe",'w') as outfile:
-	outfile.write(json.dumps(gather))
+with open("dict.bpe.de",'wb') as outfile:
+    a = json.dumps(bpe_dict)
+    outfile.write(a)
+
+# print gather
+# with open("pairstats.bpe",'wb') as outfile:
+#     a = json.dumps(gather)
+#     outfile.write(a)
